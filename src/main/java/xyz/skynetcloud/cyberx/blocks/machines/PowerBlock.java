9af3e7797a06c4b8e71f259eb1639b2133fbdc45
SkyNetCloud <mcboxer12101@gmail.com>
2018-12-28 15:07:51 -0500

    Remove/Fix Some of the Code

--- src/main/java/xyz/skynetcloud/cyberx/blocks/machines/PowerBlock.java
+++ src/main/java/xyz/skynetcloud/cyberx/blocks/machines/PowerBlock.java
@@ -3,21 +3,34 @@ package xyz.skynetcloud.cyberx.blocks.machines;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import xyz.skynetcloud.cyberx.Main;
 import xyz.skynetcloud.cyberx.blocks.BlockBase;
-import xyz.skynetcloud.cyberx.blocks.tileentities.TileEntityPowerBlock;
 
 public class PowerBlock extends BlockBase {
 
 	public PowerBlock(String name, Material material, CreativeTabs tab) {
 		super(name, material, tab);
 	}
+	
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) 
+	{
+		if(!worldIn.isRemote)
+		{
+			playerIn.sendMessage(new TextComponentString("This Block Is Still WIP"));
+		}
+		
+		return true;
+	}
+	
 	
 	@Override
 	public boolean hasTileEntity(IBlockState state) 
@@ -28,6 +41,6 @@ public class PowerBlock extends BlockBase {
 	@Override
 	public TileEntity createTileEntity(World world, IBlockState state)
 	{
-		return new TileEntityPowerBlock();
+		return null;
 	}
 }
